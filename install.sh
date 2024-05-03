fc7532186a6cef8edc492faf06bfe93afd66f7c5
Ivan Bondyrev <ivan.bondivan-bond@outlook.com>
2024-05-03 20:03:18 +0200

    installation scipt

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+INSTALL_DIR="/opt/correct_script.cli"
+
+REPO="iyubondyrev/correct_script.cli"
+
+API_URL="https://api.github.com/repos/$REPO/releases/latest"
+
+echo "Creating installation directory at $INSTALL_DIR..."
+sudo mkdir -p "$INSTALL_DIR"
+
+echo "Fetching latest release from GitHub..."
+release_data=$(curl -s "$API_URL")
+
+jar_url=$(echo "$release_data" | grep "browser_download_url.*correct_script.cli.jar" | cut -d '"' -f 4)
+
+echo "Downloading correct_script.cli.jar..."
+sudo curl -L "$jar_url" -o "$INSTALL_DIR/correct_script.cli.jar"
+
+if [ -f "$HOME/.bashrc" ]; then
+    PROFILE="$HOME/.bashrc"
+elif [ -f "$HOME/.zshrc" ]; then
+    PROFILE="$HOME/.zshrc"
+else
+    echo "No .bashrc or .zshrc found, using .bashrc by default"
+    PROFILE="$HOME/.bashrc"
+fi
+
+echo "Adding alias to the shell profile..."
+echo "alias correct_script='java -jar $INSTALL_DIR/correct_script.cli.jar'" >> $PROFILE
+
+echo "Installation completed. Please run 'source $PROFILE' to reload your shell or restart your terminal."
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,7 +14,7 @@ fun main(args: Array<String>) {
     val maxRetries by parser.option(ArgType.Int, shortName = "r", fullName = "max_retires", description = "Maximum retry attempts").default(5)
     val pythonInterpreter by parser.option(ArgType.String, shortName = "pi", fullName = "python_interpreter",
         description = "What to use when running python script, for example python, python3. Will be used like this: \"python3\" input_file.py")
-        .default("python")
+        .default("python3")
     val timeout by parser.option(ArgType.Int, shortName = "t", fullName = "timeout",
         description = "How long to wait the server to respond in seconds")
         .default(60)
